40a876ef8da935a0df9e841b629e76128d8d20c4
Andrii Savka <andrii2353@gmail.com>
2023-11-28 17:53:16 +0200

    Add committing user answers

--- Client/src/components/TestPage/QuestionBlock/QuestionBlockComponent.tsx
+++ Client/src/components/TestPage/QuestionBlock/QuestionBlockComponent.tsx
@@ -1,16 +1,17 @@
 import RadioGroup from "@mui/material/RadioGroup";
-import { Question } from "../../types";
+import { Answer, Question } from "../../types";
 import "./QuestionBlockComponentStyle.css";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import { Radio } from "@mui/material";
+import { useState } from "react";
 
-export default function QuestionBlock(props: { question: Question }) {
+export default function QuestionBlock(props: { question: Question, questionIndex:number, setAnswer: (questionIndex: number, answer: Answer) => void }) {
     return (
         <div className="question-container">
             <h3>{props.question.title}</h3>
             <RadioGroup>
                 {props.question.answers.map(answer =>
-                    <FormControlLabel value={answer.id} sx={{fontSize:500}} control={<Radio />} label={answer.title} />)}
+                    <FormControlLabel key={answer.id} onClick={() => props.setAnswer(props.questionIndex, answer)} value={answer.id} sx={{ fontSize: 500 }} control={<Radio />} label={answer.title} />)}
             </RadioGroup>
         </div>
     )
--- Client/src/components/TestPage/testPageComponent.tsx
+++ Client/src/components/TestPage/testPageComponent.tsx
@@ -1,28 +1,29 @@
 import { useEffect, useState } from "react";
-import { Question, Test } from "../types";
+import { Answer, Question, Test } from "../types";
 import "./testPageComponentStyle.css";
 import { useLocation, useNavigate } from "react-router-dom";
 import TestService from "../../Services/test.service";
 import QuestionBlock from "./QuestionBlock/QuestionBlockComponent";
+import { Button } from "@mui/material";
 
 export default function TestsPage() {
     const location = useLocation();
     const navigate = useNavigate();
     const testId = new URLSearchParams(location.search).get('testId');
 
-    const [questions, setQuestions] = useState<Question[]>();
+    const [questions, setQuestions] = useState<Question[]>([]);
     const [test, setTest] = useState<Test>();
+    const [userAnswers, setUserAnswers] = useState<Answer[]>(Array.from({ length: questions?.length }));
 
     const setTestQuestions = async () => {
         let response = await TestService.GetTestQuestionsWithAnswers(Number(testId));
         setQuestions(response);
     }
 
-    const setCurrentTest = async () =>{
+    const setCurrentTest = async () => {
         let response = await TestService.GetTest(Number(testId));
         setTest(response);
     }
-    
     useEffect(() => {
         if (!testId) {
             navigate("/");
@@ -33,12 +34,25 @@ export default function TestsPage() {
         }
     }, []);
 
+    const setUserAnswer = (questionIndex: number, answer: Answer) => {
+        setUserAnswers(prevAnswers => {
+            const newAnswers = [...prevAnswers];
+            newAnswers[questionIndex] = answer;
+            return newAnswers;
+        });
+    }
+
+    useEffect(() => {
+        console.log(userAnswers)
+    }, [userAnswers]);
+
     return (
         <div className="questions-container">
             <h1>{test?.title}</h1>
             {questions?.map(question => (
-                <QuestionBlock question={question}/>
+                <QuestionBlock key={question.id} question={question} questionIndex={questions.indexOf(question)} setAnswer={setUserAnswer} />
             ))}
+            <Button>Submit</Button>
         </div>
     )
 }
